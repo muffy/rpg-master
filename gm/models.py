df6620f6a9e36f193d0b299f677e07dab56dda12
muffy <muffy@instacart.com>
2018-05-14 00:17:37 -0700

    Recreate db again.

--- gm/models.py
+++ gm/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-
+from django.contrib.postgres import fields
 
 class Player(models.Model):
     slack_id = models.CharField(max_length=20,unique=True)
     name = models.CharField(max_length=200)
 
     def __str__(self):
-        return f"{self.name or self.slack_id}"
+        return f"{self.name} ({self.slack_id})"
 
     class Meta:
         db_table = 'player'
-
+        ordering = ['name']
 
 # This can represent either a player character or an NPC
 #  - (enemy) NPCs belong to the Player who is also the GM
@@ -19,47 +19,33 @@ class Player(models.Model):
 class Character(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    initiative_modifier = models.IntegerField(default=0)
-    perception_modifier = models.IntegerField(default=0)
-    armor_class = models.IntegerField(default=10)
-    hit_points = models.IntegerField(default=0)
-    cmd = models.IntegerField(default=10)
-    will_save = models.IntegerField(default=0)
-    reflex_save = models.IntegerField(default=0)
-    fortitude_save = models.IntegerField(default=0)
+    stats = fields.JSONField(default=dict())
+    statblock = models.TextField()
+    emoji = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.player.name})"
 
     class Meta:
         db_table = 'character'
+        ordering = ['name']
 
 
 # For now, there should be one game per slack channel; we will make this more flexible later.
 class Game(models.Model):
     name = models.CharField(max_length=200)
-    ended = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
     gm = models.ForeignKey(Player, on_delete=models.CASCADE)
-    slack_channel = models.CharField(max_length=20,null=True)
+    slack_channel = models.CharField(max_length=20, null=False)
     current_round = models.IntegerField(default=0)
+    characters = models.ManyToManyField(Character)
 
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'game'
-
-
-# Characters always in the game - will be copied to each encounter in the game
-class GameCharacter(models.Model):
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    character = models.ForeignKey(Character, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.character.name}"
-
-    class Meta:
-        db_table = 'game_character'
+        ordering = ['name']
 
 
 # For one, one encounter should be running at a time.
@@ -67,13 +53,42 @@ class Encounter(models.Model):
     name = models.CharField(max_length=200)
     ended = models.BooleanField(default=False)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    start_round = models.IntegerField(default=0) # this is the game round in which the encounter started
+    first_round = models.IntegerField(default=1)
+    characters = models.ManyToManyField(Character, through='EncounterCharacter')
 
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'encounter'
+        ordering = ['-first_round']
+
+
+# Buffs, debuffs, spells, etc.
+class Adjustment(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+
+    def __str__(self):
+        return f'<a href="{self.url}">{self.name}</a>'
+
+    class Meta:
+        db_table = 'adjustment'
+        ordering = ['name']
+
+
+# Adjustments which are active in a game at the moment
+class GameAdjustment(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    adjustment = models.ForeignKey(Adjustment, on_delete=models.CASCADE)
+    characters = models.ManyToManyField(Character)
+    source = models.ForeignKey(Character, on_delete=models.CASCADE, null=True, related_name='source')
+    start_round = models.IntegerField(default=0)
+    duration = models.IntegerField(default=-1)
+
+    class Meta:
+        db_table = 'game_adjustment'
+        ordering = ['-start_round']
 
 
 # Details of an encounter - which characters, initiative, delay status, readied status, conditions, etc.
@@ -88,33 +103,4 @@ class EncounterCharacter(models.Model):
 
     class Meta:
         db_table = 'encounter_character'
-
-
-# Buffs, debuffs, spells, etc.
-class Adjustment(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    url = models.CharField(max_length=500)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        db_table = 'adjustment'
-
-
-# Adjustments which are active in a game at the moment
-# These can be just to the game or they can be active during a particular encounter or on a character or characters
-class GameAdjustment(models.Model):
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    encounter = models.ForeignKey(Encounter, on_delete=models.CASCADE, null=True)
-    character =  models.ForeignKey(Character, on_delete=models.CASCADE, null=True)
-    adjustment =  models.ForeignKey(Adjustment, on_delete=models.CASCADE)
-    start_round = models.IntegerField(default=0)
-    end_round = models.IntegerField(default=0)
-
-    def __str__(self):
-        return f"{self.character.name}: {self.adjustment.name}"
-
-    class Meta:
-        db_table = 'game_adjustment'
+        ordering = ['-initiative']
