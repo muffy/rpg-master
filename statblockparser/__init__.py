2a5c8ef6c32f9cb79e94105d4c0dc3fad5e5d869
muffy <muffy@instacart.com>
2018-05-20 02:31:50 -0700

    Distinguish PC and NPC parsing

--- statblockparser/__init__.py
+++ statblockparser/__init__.py
@@ -1,18 +1,32 @@
 from pyparsing import *
 
-ParserElement.setDefaultWhitespaceChars(" \t")
-ParentheticalRE = Optional(Regex("\(.*?\)"))
-NL = ParentheticalRE + LineEnd().suppress()
+DIVIDER = "--------------------"
+
+NL = LineEnd().suppress()
 END = StringEnd()
+
+ParserElement.setDefaultWhitespaceChars(" \t")
+ParenRE = Regex("\(.*?\)")
 SignedInteger = pyparsing_common.signed_integer
 Integer = pyparsing_common.integer
 Comma = Literal(",").suppress()
 SemiColon = Literal(";").suppress()
-SkipRest = Suppress(restOfLine) + NL
-Divider = Literal("--------------------")
-NextSection = SkipTo(Divider | END)
+Divider = Literal(DIVIDER)
+
 NameRE = Regex("[^\n,]+").setParseAction(lambda s, l, t: t[0].strip())
 NamedList = delimitedList(NameRE, delim=",")
+HyphenWord = Word(alphas + "-")
+Phrase = OneOrMore(HyphenWord).setParseAction(lambda s, l, t: " ".join(t))
+
+Gender = oneOf("Female Male")
+Alignment = oneOf("CE CN CG NE N NG LE LN LG")
+Size = oneOf("Diminutive Tiny Small Medium Large Huge Gargantuan")
+
+SectionHeader = Divider + NL + OneOrMore(Word(alphas)) + NL + Divider + NL
+NextSection = Suppress(SkipTo(SectionHeader)) + Suppress(SectionHeader)
+
+# NPC description
+Description = restOfLine("description") + NL + Suppress(Divider) + NL
 
 # Basics content
 """
@@ -21,41 +35,30 @@ Female human (Garundi) druid 9
 N Medium humanoid (human)
 Init +2; Senses Perception +12
 """
-# Line 1
 CharacterName = restOfLine.setParseAction(lambda s, l, t: t[0].split("CR")[0].strip())
 NameLine = CharacterName("character_name") + NL
 
 XPLine = Literal("XP") + restOfLine + NL
 
-# Line 2
-Genders = (Literal("Female") | Literal("Male"))
-Race = OneOrMore(Word(alphas)).setParseAction(lambda s, l, t: " ".join(t))
-RaceAndNationality = Race("race") + Suppress(Optional(ParentheticalRE))
-Class = OneOrMore(Word(alphas)).setParseAction(lambda s, l, t: " ".join(t))
-Level = Word(nums)
-ClassLevel = Group(Class("class") + Suppress(Optional(ParentheticalRE)) + Level("level"))
+ClassLevel = Group(Phrase("class") + Integer("level"))
 Classes = delimitedList(ClassLevel, delim="/")
-GRCLine = Genders("gender") + RaceAndNationality + Classes("classes") + NL
+GRCLine = Gender("gender") + HyphenWord("race") + Optional(Classes("classes")) + NL
+GRCLine.ignore(ParenRE)
 
-# Line 3
-# Alignment
-Chaotic = Literal("C")("chaotic")
-Lawful = Literal("L")("lawful")
-Evil = Literal("E")("evil")
-Good = Literal("G")("good")
-Neutral = Literal("N")("neutral")
-Legal = (Chaotic | Lawful)
-Moral = (Evil | Good | Neutral)
-Alignment = Group(Optional(Legal) + Moral)
-Sizes = (Literal("Diminutive") | Literal("Tiny") | Literal("Small") | Literal("Medium") | Literal("Large") | Literal("Huge") | Literal("Gargantuan"))
-ASTLine = Alignment("alignment") + Sizes("size") + restOfLine + NL
+ASTLine = Alignment("alignment") + Size("size") + Suppress(restOfLine) + NL
 
-# Line 4
 Initiative = Suppress(Literal("Init")) + SignedInteger("initiative")
 Perception = Suppress(Literal("Perception")) + SignedInteger("perception")
-InitLine = Initiative + SkipTo(Perception, include=True) + NL
+InitLine = Initiative + SkipTo(Perception) + Perception + NL
 
-Basics = NameLine + Optional(XPLine) + GRCLine + ASTLine + InitLine + NextSection
+Basics = NameLine + GRCLine + ASTLine + InitLine
+
+# NPC Basics
+
+GRLine = Gender("gender") + Phrase("race") + NL
+GRLine.ignore(ParenRE)
+
+NPCBasics = Description + NameLine + XPLine + GRLine + ASTLine + InitLine
 
 # Defense
 """
@@ -66,25 +69,18 @@ AC 20, touch 12, flat-footed 18 (+8 armor, +2 Dex)
 hp 75 (9d8+27)
 Fort +9, Ref +6, Will +15; +4 vs. fey and plant-targeted effects
 Immune poison"""
-DefenseHeader = Divider + NL + "Defense" + NL + Divider + NL
-
-# Line 1
 AC = Suppress(Literal("AC")) + SignedInteger("ac") + Comma
 TouchAC = Literal("touch") + SignedInteger("touch_ac") + Comma
 FlatFootedAC = Literal("flat-footed") + SignedInteger("flat_footed_ac")
 ACLine = AC + TouchAC + FlatFootedAC + Suppress(restOfLine) + NL
 
-# Line 2
 HPLine = Literal("hp") + SignedInteger("hp") + Suppress(restOfLine) + NL
 
-# Line 3
-FortSave = Suppress(Literal("Fort")) + SignedInteger("fort_save") + Comma
-ReflexSave = Suppress(Literal("Ref")) + SignedInteger("reflex_save") + Comma
-WillSave = Suppress(Literal("Will")) + SignedInteger("will_save")
-OtherSaves = Suppress(Literal(";")) + restOfLine("other_saves")
-SaveLine = FortSave + ReflexSave + WillSave + Optional(OtherSaves) + NL
+Save = Suppress(Word(alphas)) + SignedInteger + Optional(Comma)
+Save.setParseAction(lambda s, l, t: t[0])
+SaveLine = Save("fort_save") + Save("reflex_save") + Save("will_save") + Optional(SemiColon + restOfLine("other_saves"))
 
-Defense = Suppress(DefenseHeader) + ACLine + HPLine + SaveLine + Suppress(NextSection)
+Defense = NextSection + ACLine + HPLine + SaveLine
 
 # Offense
 """
@@ -92,26 +88,21 @@ Defense = Suppress(DefenseHeader) + ACLine + HPLine + SaveLine + Suppress(NextSe
 Offense
 --------------------
 Speed 30 ft. (20 ft. in armor)
-Melee hope knife +5/+0 (1d4-2/19-20) or
-   quarterstaff of entwined serpents +5/+0 (1d6-1) or
-   scimitar +4/-1 (1d6-2/18-20)
-Special Attacks wild shape 3/day
-Druid Spells Prepared (CL 9th; concentration +18)
-   5th—control winds (DC 22), cure critical wounds
-   4th—air walk, cure serious wounds, dispel magic
-   3rd—cure moderate wounds, hide campsite[APG] (DC 20), ice spears (DC 21), greater magic fang, protection from energy
-   2nd—barkskin, bull"s strength, delay poison, flaming sphere (DC 19), flurry of snowballs (DC 19), reduce animal
-   1st—ant haul[APG] (DC 18), cure light wounds, entangle (DC 18), entangle (DC 18), faerie fire, longstrider
-   0 (at will)—create water, detect magic, light, stabilize
+[...]
 """
-OffenseHeader = Divider + NL + "Offense" + NL + Divider + NL
+NumFeet = Integer + Suppress(Literal("ft."))
+NumFeet.setParseAction(lambda s, l, t: f"{t[0]}")
+SpeedLine = Suppress(Literal("Speed")) + NumFeet + Optional("(" + NumFeet) + restOfLine + NL
+SpeedLine.setParseAction(lambda s, l, t: "/".join(t))
 
-# Line 1
-Speed = Suppress(Literal("Speed")) + SignedInteger("speed") + Suppress(Literal("ft."))
-ArmorSpeed = Suppress(Literal("(")) + SignedInteger("speed_in_armor") + Suppress(Literal("ft.") + Literal("in") + Literal ("armor") + Literal(")"))
-SpeedLine = Speed + Optional(ArmorSpeed) + NL
+# NPC
+Space = Literal("Space")
+Reach = Literal("Reach")
+SpaceFeet = SkipTo(Space) + Suppress(Space) + NumFeet("space")
+ReachFeet = SkipTo(Reach) + Suppress(Reach) + NumFeet("reach")
 
-Offense = Suppress(OffenseHeader) + SpeedLine + Suppress(NextSection)
+Offense = NextSection + SpeedLine("speed")
+NPCOffense = NextSection + SpeedLine("speed") + SpaceFeet + ReachFeet
 
 # Statistics
 """
@@ -127,49 +118,47 @@ Languages Common, Druidic, Osiriani, Sign Language (Common), Sylvan, Terran
 SQ nature bond (tiger named Carl), nature sense, trackless step, wild empathy +7, woodland stride
 Combat Gear scroll of barkskin, scroll of barkskin, scroll of cure serious wounds, scroll of darkvision, scroll of delay poison, scroll of feather step, scroll of greater magic fang, scroll of lesser restoration, scroll of remove blindness/deafness, scroll of remove curse, scroll of remove disease, scroll of see invisibility, scroll of speak with animals, scroll of speak with plants, wand of barkskin (15 charges), wand of cure light wounds; Other Gear +2 dragonhide agile breastplate[APG], +2 light wooden shield, light wooden shield, quarterstaff of entwined serpents, hope knife, scimitar, cloak of resistance +1, headband of inspired wisdom +2, druid backpack, spell component pouch, 2,466 gp, 6 sp, 7 cp
 """
-StatisticsHeader = Divider + NL + "Statistics" + NL + Divider + NL
+Score = Suppress(Word(alphas)) + Integer + Optional(Comma)
+Score.setParseAction(lambda s, l, t: t[0])
+AbilityLine = Score("strength") + Score("dexterity") + Score("constitution") + Score("intelligence") + Score("wisdom") + Score("charisma") + NL
 
-# Line 1
-Strength = Suppress(Literal("Str")) + SignedInteger("strength") + Comma
-Dexterity = Suppress(Literal("Dex")) + SignedInteger("dexterity") + Comma
-Constitution = Suppress(Literal("Con")) + SignedInteger("constitution") + Comma
-Intelligence = Suppress(Literal("Int")) + SignedInteger("intelligence") + Comma
-Wisdom = Suppress(Literal("Wis")) + SignedInteger("wisdom") + Comma
-Charisma = Suppress(Literal("Cha")) + SignedInteger("charisma")
-AbilityLine = Strength + Dexterity + Constitution + Intelligence + Wisdom + Charisma + NL
-
-# Line 2
-CMB = Suppress(SkipTo(Literal("CMB")) + Literal("CMB")) + SignedInteger("cmb") + ParentheticalRE + SemiColon
-CMD = Suppress(Literal("CMD")) + Integer("cmd") + ParentheticalRE
+CMB = Suppress(SkipTo(Literal("CMB")) + Literal("CMB")) + SignedInteger("cmb") + Optional(ParenRE) + SemiColon
+CMD = Suppress(Literal("CMD")) + Integer("cmd") + Optional(ParenRE)
 AttackLine = CMB + CMD + Suppress(restOfLine) + NL
 
-# Line 3
 FeatsLine = Suppress(Literal("Feats")) + NamedList("feats") + NL
+TraitsLine = Suppress(Literal("Traits")) + NamedList("traits") + NL
+TricksLine = Suppress(Literal("Tricks")) + NamedList("tricks") + NL
 
-# Line 4
-TraitsOrTricks = (Literal("Traits") | Literal("Tricks"))
-TraitsLine = Suppress(TraitsOrTricks) + NamedList("traits_or_tricks") + NL
+FeatsEtc = FeatsLine + Optional(TraitsLine) + Optional(TricksLine)
 
-# Line 5
 SkillName = Regex("[^+\-\d\n]+").setParseAction(lambda s, l, t: t[0].strip())
-SkillBonus = SignedInteger
-SkillExtra = Regex("\(.*?\)")
-Skill = Group(SkillName("skill") + SkillBonus("bonus") + Optional(SkillExtra)("extra"))
+Skill = Group(SkillName("skill") + SignedInteger("bonus") + Optional(ParenRE("extra")))
 Skills = delimitedList(Skill, delim=",")("skills")
 ExtraModifiers = SemiColon + restOfLine.suppress()
 SkillsLine = Suppress(Literal("Skills")) + Skills + Optional(ExtraModifiers) + NL
 
-# Line 6
 LanguageLine = Suppress(Literal("Languages")) + NamedList("languages") + NL
 
-# Line 7
 SQLine = Suppress(Literal("SQ")) + NamedList("sq") + NL
 
-# Line 8
-Gear = (Literal("Combat Gear") | Literal("Other Gear"))
-GearLine = Suppress(Gear) + NamedList("gear") + NL
+GearType = (Literal("Combat Gear") | Literal("Other Gear"))
+GearLine = Suppress(GearType) + NamedList("gear") + NL
 
-Statistics = Suppress(StatisticsHeader) + AbilityLine + AttackLine + FeatsLine + Optional(TraitsLine) + SkillsLine + Optional(LanguageLine) + SQLine + GearLine
+SkillsEtc = SkillsLine + Optional(LanguageLine) + Optional(SQLine) + Optional(GearLine)
+
+Statistics = NextSection + AbilityLine + AttackLine + FeatsEtc + SkillsEtc
+
+# NPC Ecology
+"""
+--------------------
+Ecology
+--------------------
+Environment cold mountains
+Organization solitary, gang (3-5), band (6-12 plus 35% noncombatants and 1 adept or cleric of 1st-2nd level), raiding party (6-12 plus 35% noncombatants, 1 adept or sorcerer of 3rd-5th level, 1-4 winter wolves, and 2-3 ogres), or tribe (21-30 plus 1 adept, cleric, or sorcerer of 6th-7th level; 1 barbarian or ranger jarl of 7th-9th level; and 15-36 winter wolves, 13-22 ogres, and 1-2 young white dragons)
+Treasure standard (chain shirt, greataxe, other treasure)
+"""
+Ecology = NextSection
 
 # Special Abilities
 """
@@ -190,16 +179,22 @@ Wild Empathy +7 (Ex) Improve the attitude of an animal, as if using Diplomacy.
 Wild Shape (9 hours, 3/day) (Su) Shapeshift into a different creature one or more times per day.
 Woodland Stride (Ex) Move through undergrowth at normal speed. 
 """
-SpecialHeader = Divider + NL + "Special Abilities" + NL + Divider + NL
 SpecialAbilityRE = Regex("[^\n]+").setParseAction(lambda s, l, t: t[0].strip())
 SpecialAbilityLine = SpecialAbilityRE + NL
 SpecialAbilities = ZeroOrMore(SpecialAbilityLine)("special_abilities")
 
-Special = Suppress(SpecialHeader) + SpecialAbilities
+Special = NextSection + SpecialAbilities
 
-StatBlock = Basics + Defense + Offense + Statistics + Special
+PCStatBlock = Basics + Defense + Offense + Statistics + Special
+NPCStatBlock = NPCBasics + Defense + NPCOffense + Statistics + Ecology + Special
+
 
 def parse_statblock(statblock):
-    return StatBlock.parseString(statblock).asDict()
+    lines = statblock.split("\n")
+    if lines[1] == DIVIDER:
+        # This is an NPC statblock
+        return NPCStatBlock.parseString(statblock).asDict()
+    else:
+        return PCStatBlock.parseString(statblock).asDict()
 
 
